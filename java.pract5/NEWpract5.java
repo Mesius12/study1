b6b9dd9df8c08c9319dec311f8666f04455ce307
Mesius12 <145146888+Mesius12@users.noreply.github.com>
2024-10-02 18:26:29 +0300

    Update NEWpract5.java

--- java.pract5/NEWpract5.java
+++ java.pract5/NEWpract5.java
@@ -1,16 +1,29 @@
+class NoObject {
+    private int num;
+    private String word;
+    public NoObject(int num)  {
+        this.num = num;
+    }
+    public NoObject(String word) {
+        this.word = word;
+    }
+    public String toString() {
+        return (word != null) ? word : String.valueOf(num);
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        int[] Numeros = {1,3,5,7};
-        String[] Words = {"second","quatro","six","vosem"};
-        int razmer = Numeros.length + Words.length;
-        Object[] CompletedArr = new Object[razmer];
+        int[] Numbers = {1,3,5,7};
+        String[] Words = {"second","fourth","six","eighth"};
+        int razmer = Numbers.length + Words.length;
+        NoObject[] CompletedArr = new NoObject[razmer];
 
         for (int i = 0; i < razmer; i++) {
             if (i % 2 == 0) {
-                CompletedArr[i] = Numeros[i/2];
+                CompletedArr[i] = new NoObject(Numbers[i/2]);
             }
             else {
-                CompletedArr[i] = Words[i/2];
+                CompletedArr[i] = new NoObject(Words[i/2]);
             }
         }
         for (int i = 0; i < CompletedArr.length; i++) {
